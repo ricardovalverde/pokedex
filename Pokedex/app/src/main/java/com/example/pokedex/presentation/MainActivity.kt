55a363f22c87a4a2793c7c933848ca4f2d032530
ricardovalverde <valverde.ricardojr@gmail.com>
2021-09-03 02:31:25 -0300

    update 03-SET

---
+++ Pokedex/app/src/main/java/com/example/pokedex/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.pokedex.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.pokedex.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
